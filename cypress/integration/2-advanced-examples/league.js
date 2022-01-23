589b738f17a712eb3df63281c6179aa6efc6cc41
Ben Spurlock <spurlock114@gmail.com>
2022-01-23 12:47:34 -0700

    fixed ur shite

--- cypress/integration/2-advanced-examples/league.js
+++ cypress/integration/2-advanced-examples/league.js
@@ -1,57 +1,26 @@
 const requiredLeague = require("../../fixtures/league");
 
 context("Files", () => {
-  beforeEach(() => {
-    cy.visit("https://maps.littleleague.org/leaguefinder/");
-  });
-
-  it("loads the same object", () => {
-    cy.fixture("league").then((leagueFixture) => {
-      expect(requiredLeague, "the same data").to.deep.equal(leagueFixture);
-    });
-  });
-
   describe("Find the league", () => {
     beforeEach(() => {
       cy.visit("https://maps.littleleague.org/leaguefinder/");
     });
-    it("enter details and submit", () => {
-      cy.fixture("league").then((leagueFixture) => {
-        leagueFixture.forEach((kid) => {
-          cy.log(kid)
-          if (kid.baseballAge !== null) {
-            cy.get("#sport-type-input").select("Baseball");
-          } else {
-            cy.get("#sport-type-input").select("Softball");
-          }
-          cy.get("#address-input").type(
-            kid.address + " " + kid.city + " " + kid.state + " " + kid.zip
-          );
-          cy.get('#search-button').click();
-          it("should have a link to the league", () => {
-            cy.get(".col-md-12 > .header").should("contain", "WALNUT CREEK LL");
-          });
 
-          // in a real test you probably need to do some kind of assertion here
-        });
+    for (let i = 0; i < requiredLeague.length; i++) {
+      let kid = requiredLeague[i];
+      it(`${kid.lastname}, ${kid.firstname}`, () => {
+        cy.log(kid.firstname, kid.lastname);
+        if (kid.baseballAge !== null) {
+          cy.get("#sport-type-input").select("Baseball");
+        } else {
+          cy.get("#sport-type-input").select("Softball");
+        }
+        cy.get("#address-input").type(
+          kid.address + " " + kid.city + " " + kid.state + " " + kid.zip
+        );
+        cy.get("#search-button").click();
+        cy.get(".col-md-12 > .header").should("contain", "WALNUT CREEK LL");
       });
-    });
+    }
   });
-
-  // it("should loop through each kid", () => {
-  //   requiredLeague.forEach((kid) => {
-  //     if (kid.baseballAge !== null) {
-  //       cy.get("#sport-type-input").select("Baseball");
-  //     } else {
-  //       cy.get("#sport-type-input").select("Softball");
-  //     }
-  //     cy.get("#address-input").type(
-  //       kid.address + " " + kid.city + " " + kid.state + " " + kid.zip
-  //     );
-  //     // cy.get('#search-button').click();
-  //     it("should have a link to the league", () => {
-  //       cy.get(".col-md-12 > .header").should("contain", "WALNUT CREEK LL");
-  //     });
-  //   });
-  // });
 });
